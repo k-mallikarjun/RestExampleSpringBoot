55e359e259763c19dbd13bb218d1207e13792ce5
k-mallikarjun <k.mallikarjun113@gmail.com>
2023-05-29 13:24:52 +0530

    Initial Commit

---
+++ src/main/java/com/example/RestExampleSpringBoot/RestExampleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestExampleSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestExampleSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestExampleSpringBoot/RestExampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestExampleSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
